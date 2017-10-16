f86d14ed367f1e9a9156b43fa5118a1297081309
Hugo Durães <hugoduraes@gmail.com>
2017-10-17 00:59:16 +0100

    add increment method and tests for it

---
+++ test/increment.test.js
@@ -0,0 +1,13 @@
+const { increment } = require('../lib');
+
+test('increment of 0 to equal 1', () => {
+  expect(increment(0)).toBe(1);
+});
+
+test('increment of 1 to equal 2', () => {
+  expect(increment(1)).toBe(2);
+});
+
+test('increment of -1 to equal 0', () => {
+  expect(increment(-1)).toBe(0);
+});
