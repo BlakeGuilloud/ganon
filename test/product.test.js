0ce9b0d2a6abd36b04a0f7c12c7a960f4ecb4b99
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2017-10-16 16:00:25 -0700

    changed minor multiplication typo in comments

--- test/product.test.js
+++ test/product.test.js
@@ -8,6 +8,6 @@ test('multiplies 3 * 4 to equal 12', () => {
   expect(product(3, 4)).toBe(12);
 });
 
-test('multiplies 1.5 * 1.5 to equal 12', () => {
+test('multiplies 1.5 * 1.5 to equal 2.25', () => {
   expect(product(1.5, 1.5)).toBe(2.25);
 });
