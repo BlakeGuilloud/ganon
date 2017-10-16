873b3e6d0e4242872ce5e6a41a2786f170c3f8ad
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-16 17:06:49 -0400

    Wiping the old project.

--- src/emailHelpers.js
+++
@@ -1,9 +0,0 @@
-export function matchEmail(object) {
-  const returnVal = object && object.value && matchEmailRegex(object.value);
-
-  return returnVal;
-}
-
-export function matchEmailRegex(value) {
-  return !value.match(/.+?@.+?\..+?/i);
-}
