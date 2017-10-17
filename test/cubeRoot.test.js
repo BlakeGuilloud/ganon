a01a3882ef45d1a047286bbe7e8c9b8df5044322
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2017-10-18 00:38:04 +0400

    Skeleton-method for #13

--- test/cubeRoot.test.js
+++ test/cubeRoot.test.js
@@ -1,9 +1,9 @@
 const { cubeRoot } = require('../lib');
 
-test('cube root of 27 to equal 3', () => {
+test('cube-root of 27 to equal 3', () => {
   expect(squareRoot(27)).toBe(3);
 });
 
-test('cube root of 125 to equal 5', () => {
+test('cube-root of 125 to equal 5', () => {
   expect(cubeRoot(125)).toBe(5);
 });
