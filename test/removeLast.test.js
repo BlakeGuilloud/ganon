49054135e3bd0215121ab44bbc6fa6ae5121f25f
Ryan Kelley <rtkelley@gmail.com>
2017-10-18 15:33:01 -0400

    updated index and test files for removeLast

--- test/removeLast.test.js
+++ test/removeLast.test.js
@@ -1,13 +1,13 @@
 const { removeLast } = require("../lib");
 
 test("duck is removed from array", () => {
-  expect(removeLast(["cat", "dog", "fish", "duck"])).toBe(["cat", "dog", "fish"]);
+  expect(removeLast(["cat", "dog", "fish", "duck"])).toEqual(["cat", "dog", "fish"]);
 });
 
 test("d is removed from array", () => {
-  expect(removeLast(["a", "b", "c", "d"])).toBe(["a", "b", "c"]);
+  expect(removeLast(["a", "b", "c", "d"])).toEqual(["a", "b", "c"]);
 });
 
 test("4 is removed from array", () => {
-  expect(removeLast([1, 2, 3, 4])).toBe([1, 2, 3]);
+  expect(removeLast([1, 2, 3, 4])).toEqual([1, 2, 3]);
 });
