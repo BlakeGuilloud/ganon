c108e7ec8d0ea3c33c6e26fc8cd8136030967e6a
Aaron LeCheminant <aaronjlech@gmail.com>
2017-10-18 15:30:19 -0400

    solved for decrement, fixed decrement tests, added skeleton and tests for indexOf

--- test/decrement.test.js
+++ test/decrement.test.js
@@ -1,13 +1,13 @@
 const { decrement } = require('../lib');
 
 test('decrement of 1 to equal 0', () => {
-  expect(increment(1)).toBe(0);
+  expect(decrement(1)).toBe(0);
 });
 
 test('increment of 2 to equal 1', () => {
-  expect(increment(2)).toBe(1);
+  expect(decrement(2)).toBe(1);
 });
 
 test('increment of 0 to equal -1', () => {
-  expect(increment(0)).toBe(-1);
+  expect(decrement(0)).toBe(-1);
 });
