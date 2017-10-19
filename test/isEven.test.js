b1afa3c95b28026a917e3d9915d8fa1c9bbca64e
damjanh <damjan.hudobreznik@gmail.com>
2017-10-19 19:04:49 +0200

    Group unit tests in test suites #152  - Resolve issue #152. Added missing describe calls in all test files.

--- test/isEven.test.js
+++ test/isEven.test.js
@@ -1,9 +1,11 @@
 const { isEven } = require("../lib");
 
-test("10 is even", () => {
-  expect(isEven(10)).toBe(true);
-});
+describe("isEven", () => {
+  test("10 is even", () => {
+    expect(isEven(10)).toBe(true);
+  });
 
-test("5 is not even", () => {
-  expect(isEven(5)).toBe(false);
+  test("5 is not even", () => {
+    expect(isEven(5)).toBe(false);
+  });
 });
--- test/quotient.test.js
+++ test/quotient.test.js
@@ -1,9 +1,11 @@
 const { quotient } = require("../lib");
 
-test("divides 1 / 2 to equal .5", () => {
-  expect(quotient(1, 2)).toBe(.5);
-});
+describe("quotient", () => {
+  test("divides 1 / 2 to equal .5", () => {
+    expect(quotient(1, 2)).toBe(.5);
+  });
 
-test("divides 12 / 4 to equal 3", () => {
-  expect(quotient(12, 4)).toBe(3);
+  test("divides 12 / 4 to equal 3", () => {
+    expect(quotient(12, 4)).toBe(3);
+  });
 });
