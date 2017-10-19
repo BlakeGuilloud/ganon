d9a80712112e99d80f63ddebb7caeb0046641120
Luis M. Villalba <luismvillalba@gmail.com>
2017-10-19 10:48:40 -0500

    Fixing test

--- test/arrayDifference.test.js
+++ test/arrayDifference.test.js
@@ -4,6 +4,6 @@ test("arrayDifference([2, 1], [2, 3]) should return [1]", () => {
   expect(arrayDifference([1, 2], [2, 3])).toEqual([1]);
 });
 
-test("arrayDifference([2, 3, 1, 4], [2, 3]) should return [3, 1]", () => {
+test("arrayDifference([2, 3, 1, 4], [2, 3]) should return [1, 4]", () => {
   expect(arrayDifference([2, 3, 1, 4], [2, 3])).toEqual([3, 1]);
 });
