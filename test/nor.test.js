792b2e52da3ef40ce89b458d8d6e7f7abdfe2b8a
kyle tilman <kyletilman@gmail.com>
2017-10-19 16:03:08 -0400

    Implement nor function (#161)
    The tests were wrong for a nor function so I fixed those too.
    
    I also added a skeleton and tests for the first function. I would
    normally call it `head` but since `last` was already done I figured it
    should match that.
    
    Issue #97

--- test/nor.test.js
+++ test/nor.test.js
@@ -2,18 +2,18 @@ const { nor } = require("../lib");
 
 describe("nor", () => {
   test("nor false and true should be false", () => {
-    expect(nor(0, 1)).toBe(1);
+    expect(nor(0, 1)).toBeFalsy();
   });
 
   test("nor false and false should be true", () => {
-    expect(nor(0, 0)).toBe(0);
+    expect(nor(0, 0)).toBeTruthy();
   });
 
   test("nor true and false should be false", () => {
-    expect(nor(1, 0)).toBe(1);
+    expect(nor(1, 0)).toBeFalsy();
   });
 
   test("nor true and true should be true", () => {
-    expect(nor(1, 1)).toBe(0);
+    expect(nor(1, 1)).toBeFalsy();
   });
 });
