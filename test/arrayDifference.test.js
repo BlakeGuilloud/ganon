9bb591e3b3283c1af318c285d5d4664e178fe9ad
Andreas <andreasgruenh@users.noreply.github.com>
2017-10-19 17:58:40 +0200

    Fixes typo

--- test/arrayDifference.test.js
+++ test/arrayDifference.test.js
@@ -5,5 +5,5 @@ test("arrayDifference([2, 1], [2, 3]) should return [1]", () => {
 });
 
 test("arrayDifference([2, 3, 1, 4], [2, 3]) should return [1, 4]", () => {
-  expect(arrayDifference([2, 3, 1, 4], [2, 3])).toEqual([1, 4);
+  expect(arrayDifference([2, 3, 1, 4], [2, 3])).toEqual([1, 4]);
 });
