26a1466ca8e121252fcc0e268fc129e9bf445d08
Christoph Reinbothe <c_k_r@gmx.de>
2017-10-19 10:37:55 +0200

    - Fixing linting problems, removed some tests from sign, provided two new skeleton functions

---
+++ test/isPositiveZero.test.js
@@ -0,0 +1,17 @@
+const { isPositiveZero } = require("../lib");
+
+test("isPositiveZero returns true for 0", () => {
+  expect(isPositiveZero(0)).toBe(true);
+});
+
+test("isPositiveZero returns false for -0", () => {
+  expect(isPositiveZero(-0)).toBe(false);
+});
+
+test("isPositiveZero returns false for NaN", () => {
+  expect(isPositiveZero(NaN)).toBe(false);
+});
+
+test("isPositiveZero returns false for any number != 0", () => {
+  expect(isPositiveZero(42)).toBe(false);
+});
--- test/sign.test.js
+++ test/sign.test.js
@@ -1,24 +1,13 @@
 const { sign } = require("../lib");
 
-test("Sign of a negative number !=-0 should be -1", () => {
-    expect(sign(-123)).toBe(-1);
+test("Sign of a negative number should be -1", () => {
+  expect(sign(-123)).toBe(-1);
 });
 
-test("Sign of a postive number !=0 should be 1", () => {
-    expect(sign(123)).toBe(1);
+test("Sign of a postive number should be 1", () => {
+  expect(sign(123)).toBe(1);
 });
 
 test("Sign of a NaN should be NaN", () => {
-    expect(sign(NaN)).toBe(NaN);
+  expect(sign(NaN)).toBe(NaN);
 });
-
-test("Sign of a -0 should be -0", () => {
-    expect(sign(-0)).toBe(-0);
-});
-
-test("Sign of a 0 should be 0", () => {
-    expect(sign(0)).toBe(0);
-});
-
-
-
