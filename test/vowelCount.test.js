7a1570cd98bf077d6b904d7f3bd6574b9b9d69c4
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-19 17:52:54 -0400

    resolving some conflicts for @cjhaviland

--- test/vowelCount.test.js
+++ test/vowelCount.test.js
@@ -1,17 +1,19 @@
 const { vowelCount } = require("../lib");
 
-test("Number of vowels in apple is 2", () => {
-  expect(vowelCount("apple")).toBe(2);
-});
+describe("vowelCount", () => {
+  test("Number of vowels in apple is 2", () => {
+    expect(vowelCount("apple")).toBe(2);
+  });
 
-test("Number of vowels in Oranges is 3", () => {
-  expect(vowelCount("Oranges")).toBe(3);
-});
+  test("Number of vowels in Oranges is 3", () => {
+    expect(vowelCount("Oranges")).toBe(3);
+  });
 
-test("Number of vowels in gypsy is 0", () => {
-  expect(vowelCount("gypsy")).toBe(0);
-});
+  test("Number of vowels in gypsy is 0", () => {
+    expect(vowelCount("gypsy")).toBe(0);
+  });
 
-test("Number of vowels in AIEEE is 5", () => {
-  expect(vowelCount("AIEEE")).toBe(5);
+  test("Number of vowels in AIEEE is 5", () => {
+    expect(vowelCount("AIEEE")).toBe(5);
+  });
 });
