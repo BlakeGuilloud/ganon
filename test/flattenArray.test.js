0ea68a8ffed5e09a3ab46e18860cbbd76801fcbf
Thomas Wood <tom@yesbookit.com>
2017-10-20 10:27:54 +1100

    Add in isPalindrome function (#179) - Added in Skeleton for flattenArray

---
+++ test/flattenArray.test.js
@@ -0,0 +1,7 @@
+const { flattenArray } = require( "../lib" );
+
+describe( "flattenArray", () => {
+  test( "flattenArray returns flat Array", () => {
+    // expect( flattenArray() ).toBe();
+  });
+});
