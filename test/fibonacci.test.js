259185236dd59d6c2ac184a5fa40533d96ff17d9
Edgard Mota <edgardmota@gmail.com>
2017-10-18 22:05:30 -0200

    Fixed fibonacci test with minor adjustment on fibonacci function implementation

--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -1,17 +1,24 @@
 const { fibonacci } = require('../lib');
 
-test('first Fibonacci number (index 0) is 0', () => {
-  expect(fibonacci(0)).toBe(1);
+test('First Fibonacci number (index 0) is 0', () => {
+    expect(fibonacci(0)).toBe(1);
 });
 
 test('Tenth Fibonacci number (index 9) is 34', () => {
-  expect(fibonacci(9)).toBe(34);
+    expect(fibonacci(9)).toBe(34);
 });
 
-test('Hundedth Fibonacci number (index 99) is 218922995834555169026', () => {
-  expect(fibonacci(99)).toBe(218922995834555169026);
+//Integers (numbers without a period or exponent notation) are accurate up to 15 digits (https://www.w3schools.com/js/js_numbers.asp).
+test('Hundredth Fibonacci number (index 99) produces error - solution is 218922995834555169026, but JS can\'t represent it precisely', () => {
+    function fibonacci_overflow_test() {
+        fibonacci(99);
+    }
+    expect(fibonacci_overflow_test).toThrowError('Solution overflows JavaScript integer');
 });
 
 test('Negative index produces error', () => {
-  expect(fibonacci(-1)).toThrowError('Index cannot be negative');
+    function fibonacci_negative_test() {
+        fibonacci(-1);
+    }
+    expect(fibonacci_negative_test).toThrowError('Index cannot be negative');
 });
