ae9d7a9680c37d943d3a6f95d864f19ae0f70f02
Marshall Miller <marshalldavidmiller@gmail.com>
2017-10-19 21:53:22 -0700

    Completed invert, added getDuplicates (#202)

--- test/invert.test.js
+++ test/invert.test.js
@@ -1,5 +1,6 @@
 const { invert } = require("../lib");
 const testObj = {one: "1", two: "2"};
+const testString = "Hello World";
 
 describe("invert", () => {
   test("inverts object", () => {
@@ -8,5 +9,9 @@ describe("invert", () => {
 
   test("double invert gets you back where you started", () => {
     expect(invert(invert(testObj))).toEqual(testObj);
+  });
+
+  test("non-objects just get returned", () => {
+    expect(invert(testString)).toEqual("Hello World");
   });
 });
