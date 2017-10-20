00db5aad12006b1475167cf1de1f53c84289266b
buoyantair <buoyantair@gmail.com>
2017-10-20 21:30:29 +0530

    Fix & Implement regexReplace (#209)
    * Fix & Implement regexReplace
    
    * Fixed function arguments, small bug fix
    
    * Fixed errors
    
    fixed the error caused from rebase commit.

--- test/regexReplace.test.js
+++ test/regexReplace.test.js
@@ -1,15 +1,15 @@
 const { regexReplace } = require("../lib");
 
 describe("regexReplace", () => {
-  test("regexReplace('foobar') should return 'testbar'", () => {
+  test("regexReplace('foobar', 'foo', 'test') should return 'testbar'", () => {
     expect(regexReplace("foobar", "foo", "test")).toEqual("testbar");
   });
 
-  test("regexReplace('foobarfoobar') should return 'testbarfoobar'", () => {
-    expect(regexReplace("foobar", "foo", "test")).toEqual("testbarfoobar");
+  test("regexReplace('foobarfoobar', 'foo', 'test') should return 'testbarfoobar'", () => {
+    expect(regexReplace("foobarfoobar", "foo", "test")).toEqual("testbarfoobar");
   });
 
-  test("regexReplace('foobarfoobar') should return 'testbartestbar'", () => {
-    expect(regexReplace("foobar", "foo", "test", 1)).toEqual("testbartestbar");
+  test("regexReplace('foobarfoobar', 'foo', 'test', 1) should return 'testbartestbar'", () => {
+    expect(regexReplace("foobarfoobar", "foo", "test", 1)).toEqual("testbartestbar");
   });
 });
