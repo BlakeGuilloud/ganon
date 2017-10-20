2e499e7feaf9e53396d8e7079610bb8028d272d6
Indira Pranabudi <indirapranabudi@gmail.com>
2017-10-20 19:40:42 -0400

    Refactor flattenNestedObject, #250

---
+++ test/isObject.test.js
@@ -0,0 +1,19 @@
+const { isObject } = require("../lib");
+
+describe("isObject", () => {
+  test("isObject returns true for an object", () => {
+    expect(isObject({ hello: "hello" })).toBe(true);
+  });
+
+  test("isObject returns true for an empty object", () => {
+    expect(isObject({})).toBe(true);
+  });
+
+  test("isObject returns false for an array", () => {
+    expect(isObject([1, 2, 3])).toBe(false);
+  });
+
+  test("isObject returns false for a string", () => {
+    expect(isObject("hello")).toBe(false);
+  });
+});
