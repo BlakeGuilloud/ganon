e43ad4068d8b1241e84a04ba0cb5e879db22d858
Brendan Beltz <brendanwebdev@gmail.com>
2017-10-19 22:48:27 -0400

    flattenArray completed. flattenDeepArray skeleton added with tests. (#194)

--- test/flattenArray.test.js
+++ test/flattenArray.test.js
@@ -2,14 +2,14 @@ const { flattenArray } = require( "../lib" );
 
 describe("flattenArray", () => {
   test("flattenArray on empty array returns empty array", () => {
-    expect( flattenArray([]) ).toBe([]);
+    expect( flattenArray([]) ).toEqual([]);
   });
 
   test("flattenArray on already flat array returns the same array", () => {
-    expect( flattenArray([0, 1, 2, 3, 4, 5]) ).toBe([0, 1, 2, 3, 4, 5]);
+    expect( flattenArray([0, 1, 2, 3, 4, 5]) ).toEqual([0, 1, 2, 3, 4, 5]);
   });
 
   test( "flattenArray on nested array returns flat array", () => {
-    expect( flattenArray([[0, 1], [2, 3], [4, 5]]) ).toBe([0, 1, 2, 3, 4, 5]);
+    expect( flattenArray([[0, 1], [2, 3], [4, 5]]) ).toEqual([0, 1, 2, 3, 4, 5]);
   });
 });
---
+++ test/flattenDeepArray.test.js
@@ -0,0 +1,21 @@
+const { flattenDeepArray } = require( "../lib" );
+
+describe("flattenDeepArray", () => {
+  test("flattenDeepArray on empty array returns empty array", () => {
+    expect( flattenDeepArray([]) ).toEqual([]);
+  });
+
+  test("flattenDeepArray on non-array type to throw an error", () => {
+    expect(flattenDeepArray({a: 1, b: 2})).toThrow();
+    expect(flattenDeepArray("I am a banana")).toThrow();
+    expect(flattenDeepArray(4)).toThrow();
+  });
+
+  test("flattenDeepArray on already flat array returns the same array", () => {
+    expect( flattenDeepArray([0, 1, 2, 3, 4, 5]) ).toEqual([0, 1, 2, 3, 4, 5]);
+  });
+
+  test( "flattenDeepArray on nested array returns flat array", () => {
+    expect( flattenDeepArray([[[0, [1]], [2, 3]], [4, [5]]]) ).toEqual([0, 1, 2, 3, 4, 5]);
+  });
+});
