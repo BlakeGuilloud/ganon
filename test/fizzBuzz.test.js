6902a4b1b2aa91619b7e8513481fd04bee0668b4
Phillip Thomas <pyromaniac931@yahoo.com>
2017-10-20 23:31:40 -0400

    Fixed spacing and double quotes

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -9,18 +9,18 @@ describe("fizzBuzz", () => {
   });
 
   test("Number 9 is Multiple of only 3 returns 'Fizz'", () => {
-    expect(fizzBuzz(9)).toBe('Fizz');
+    expect(fizzBuzz(9)).toBe("Fizz");
   });
 
   test("Number 10 is Multiple of only 3 returns 'Buzz'", () => {
-    expect(fizzBuzz(10)).toBe('Buzz');
+    expect(fizzBuzz(10)).toBe("Buzz");
   });
 
   test("Number 15 is Multiple of 3 and 5 returns 'FizzBuzz'", () => {
-    expect(fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
   });
 
   test("Number 180 is Multiple of 3 and 5 returns 'FizzBuzz'", () => {
-    expect(fizzBuzz(180)).toBe('FizzBuzz');
+    expect(fizzBuzz(180)).toBe("FizzBuzz");
   });
 });
