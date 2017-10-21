d2a0fdcf62c9a7da692c14df45d6be5720dceb13
Phillip Thomas <pyromaniac931@yahoo.com>
2017-10-21 11:17:23 -0400

    Renamed prime.js and it's associated tests to isPrime.js, Now requiring isPrime.js in countPrimes to prevent code duplication

---
+++ test/isPrime.test.js
@@ -0,0 +1,19 @@
+const { isPrime } = require("../lib");
+
+describe("isPrime", () => {
+  test("7 is prime", () => {
+    expect(isPrime(7)).toBe(true);
+  });
+
+  test("1 is not prime", () => {
+    expect(isPrime(1)).toBe(false);
+  });
+
+  test("49 is not prime", () => {
+    expect(isPrime(49)).toBe(false);
+  });
+
+  test("95279 is prime", () => {
+    expect(isPrime(95279)).toBe(true);
+  });
+});
