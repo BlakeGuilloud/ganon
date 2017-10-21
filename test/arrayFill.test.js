eb6a5daf3bdcbbfcb364a7b30c12b23afac849a8
amoradi <aaron.moradi@daveramsey.com>
2017-10-20 22:44:48 -0500

    replace toBe with toEqual (remove exact equality checks)

--- test/arrayFill.test.js
+++ test/arrayFill.test.js
@@ -5,19 +5,19 @@ describe("arrayFill", () => {
     var foo = [1, 6, 7, "cake"];
     var bar = arrayFill(foo, "p", 1, 3);
 
-    expect(bar).not.toBe(foo);
+    expect(bar).not.toEqual(foo);
   });
 
   test("should fill new array", () => {
-    expect(arrayFill([1,2,3,4], "r")).toBe(["r","r","r","r"]);
+    expect(arrayFill([1,2,3,4], "r")).toEqual(["r","r","r","r"]);
   });
 
   test("should fill new array", () => {
-    expect(arrayFill([1,2,3,4], "r", 0)).toBe(["r","r","r","r"]);
+    expect(arrayFill([1,2,3,4], "r", 0)).toEqual(["r","r","r","r"]);
   });
 
   test("should fill new array from position 0 to 2 (not including)", () => {
     expect(arrayFill([{}, null, 6, "red", 22], "r", 0, 2))
-      .toBe(["r", "r", 6, "red", 22]);
+      .toEqual(["r", "r", 6, "red", 22]);
   });
 });
