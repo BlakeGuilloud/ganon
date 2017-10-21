19c77b1ea7f6ef5be36df49abfa83477a8761db8
Yoav Maman <yoavmmn@gmail.com>
2017-10-22 01:18:13 +0300

    fix uniq function (#312)
    * fixed uniq function
    
    * changed strict comparison to abstract comparison
    
    * added arrayLowercse skeleton method
    
    * added new test
    
    * fixed typo

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -14,11 +14,12 @@ describe("uniq", () => {
   });
 
   it("should return array with one element (the input) when given an iterable of size 1", () => {
-    expect(uniq([1])).uniq([[1]]);
+    expect(uniq([1])).toEqual([1]);
   });
 
   it("should return a unique array given an array with different items", () => {
-    expect(uniq([1,1,2,2,3,3])).toBe([1,2,3]);
-    expect(uniq(["1","1","2","2","3","3"])).toBe(["1","2","3"]);
+    expect(uniq([1,1,2,2,3,3])).toEqual([1,2,3]);
+    expect(uniq(["1","1","2","2","3","3"])).toEqual(["1","2","3"]);
+    expect(uniq(["1", 1, "2", "2", 2, 3, "3"])).toEqual(["1", 1, "2", 2, 3, "3"]);
   });
 });
