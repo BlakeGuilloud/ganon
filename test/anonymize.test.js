e21fd5261817a146f6a18c9ce654c3d217b5bab9
Demetri <developerDemetri@users.noreply.github.com>
2017-10-21 18:16:59 -0700

    ToFixed (#323)
    * filled in toFixed method
    
    * added Anonymize method
    
    * almost forgot the export

---
+++ test/anonymize.test.js
@@ -0,0 +1,21 @@
+const { anonymize } = require("../lib");
+
+describe("anonymize", () => {
+
+  test("anonymize empty name", () => {
+    expect(anonymize("")).toBe("");
+  });
+
+  test("anonymize single name", () => {
+    expect(anonymize("John")).toBe("J***");
+  });
+
+  test("anonymize full name", () => {
+    expect(anonymize("John Doe")).toBe("J*** D**");
+  });
+
+  test("anonymize middle name", () => {
+    expect(anonymize("John Smith Doe")).toBe("J*** S**** D**");
+  });
+
+});
