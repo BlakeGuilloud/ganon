bd2005263f948e00062c7f60074eefe77b95fedf
Manny1995 <immanuelda@gmail.com>
2017-10-21 22:53:26 -0700

    fixed symmetric difference, added area of rectangle method

--- test/symmetricDifference.test.js
+++ test/symmetricDifference.test.js
@@ -9,12 +9,12 @@ describe("symmetricDifference", () => {
   });
 
   it("should return the correct symmetric difference", () => {
-    expect(() => symmetricDifference([1, 2, 3, 4], [1, 4, 5])).toEqual([2, 3, 5]);
-    expect(() => symmetricDifference([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
-    expect(() => symmetricDifference([1, 2, 3], [1, 2, 3])).toEqual([]);
-    expect(() => symmetricDifference([], [1, 2, 3])).toEqual([1, 2, 3]);
-    expect(() => symmetricDifference([1, 2, 3], [])).toEqual([1, 2, 3]);
-    expect(() => symmetricDifference([], [])).toEqual([]);
+    expect(symmetricDifference([1, 2, 3, 4], [1, 4, 5])).toEqual([2, 3, 5]);
+    expect(symmetricDifference([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(symmetricDifference([1, 2, 3], [1, 2, 3])).toEqual([]);
+    expect(symmetricDifference([], [1, 2, 3])).toEqual([1, 2, 3]);
+    expect(symmetricDifference([1, 2, 3], [])).toEqual([1, 2, 3]);
+    expect(symmetricDifference([], [])).toEqual([]);
   });
 });
 
