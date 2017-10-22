e6358f9f7dd6b9031299c4b9c036303af651a13d
Joseph Hunt <josephb.hunt@gmail.com>
2017-10-22 00:57:09 -0400

    Added tests and type check.

--- test/camelCase.test.js
+++ test/camelCase.test.js
@@ -16,4 +16,12 @@ describe("camelCase", () => {
   test("A string with a 1 in it", () => {
     expect(camelCase("A string with a 1 in it")).toBe("aStringWithA1InIt");
   });
+
+  test("Throws an error when type is not allowed", () => {
+    expect(() => camelCase(1)).toThrow();
+    expect(() => camelCase(0.5)).toThrow();
+    expect(() => camelCase(null)).toThrow();
+    expect(() => camelCase(undefined)).toThrow();
+    expect(() => camelCase({})).toThrow();
+  });
 });
