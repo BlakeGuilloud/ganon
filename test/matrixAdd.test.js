99f213d92b916381f01ba60262fcbf5d96560753
Enrique Barragan <ebarragan1997@gmail.com>
2017-10-22 23:25:13 -0500

    matrixAdd method and lcm skeleton

--- test/matrixAdd.test.js
+++ test/matrixAdd.test.js
@@ -14,11 +14,11 @@ const { matrixAdd } = require("../lib");
 
 describe("matrixAdd", () => {
   test("matrixAdd(A, B) where A & B are matrices with identical dimensions (2d, 3d, 4d, ... nd), should return matrix C which is the sum of A & B", () => {
-    expect(matrixAdd([[1, 3, 1], [1, 0, 0]], [[0, 0, 5], [7, 5, 0]])).toBe([[1, 3, 6], [8, 5, 0]]);
-    expect(matrixAdd([[1, 3], [1, 0], [1, 2]], [[0, 0], [7, 5], [2, 1]])).toBe([[1, 3], [8, 5], [3, 3]]);
+    expect(matrixAdd([[1, 3, 1], [1, 0, 0]], [[0, 0, 5], [7, 5, 0]])).toEqual([[1, 3, 6], [8, 5, 0]]);
+    expect(matrixAdd([[1, 3], [1, 0], [1, 2]], [[0, 0], [7, 5], [2, 1]])).toEqual([[1, 3], [8, 5], [3, 3]]);
   });
   test("matrixAdd(A) where A is a matrix of n dimensions should return A", ()=>{
-    expect(matrixAdd([[1,2], [2,3]])).toBe([[1,2], [2,3]]);
+    expect(matrixAdd([[1,2], [2,3]])).toEqual([[1,2], [2,3]]);
   });
   test("matrixAdd(B) where B is a number | string | unsupported object should return 0", ()=>{
     expect(matrixAdd(2)).toBe(0);
