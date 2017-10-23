d7646720a128690725b15a8b87130805218bf745
MahikanthNag <babanag95@gmail.com>
2017-10-23 12:30:31 +0530

    added code for lib/lcm.js added a method signature for counting number of words in a string added test file for the above method test/countNoOfWords.test.js

---
+++ test/countNoOfWords.test.js
@@ -0,0 +1,14 @@
+const { countNoOfWords } = require("../lib");
+
+describe("countNoOfWords", () => {
+  test("countNoOfWords(A) where A is a valid string and returns C which is the count of number of words in the stirng.", () => {
+    expect(countNoOfWords("A quick brown fox jumped high over ten little lazy dogs")).toEqual(11);
+    expect(countNoOfWords("this is a simple, statement !")).toEqual(5);
+  });
+  test("countNoOfWords() where no arguments are given", ()=>{
+    expect(countNoOfWords()).toEqual(0);
+  });
+  test("countNoOfWords(A) where A is not a string or undefined", ()=>{
+     expect(countNoOfWords(2).toEqual(0));
+  })
+});
