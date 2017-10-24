a740a0a5d588738bed3aa6b7b6f2cd01ed1449b6
Ariel Davis <ariel.z.davis@icloud.com>
2017-10-24 19:33:17 -0400

    fix rounding

--- test/convertCartesianToSpherical.test.js
+++ test/convertCartesianToSpherical.test.js
@@ -2,7 +2,7 @@ const { convertCartesianToSpherical } = require("../lib");
 
 describe("convertCartesianToSpherical", () => {
   test("Cartesian([2,3,4]) will be Spherical([5.38516,56.30993,42.03111])", () => {
-    expect(convertCartesianToSpherical([2,3,4])).toEqual([5.3851,56.3099,42.0311]);
+    expect(convertCartesianToSpherical([2,3,4])).toEqual([5.3852,56.3099,42.0311]);
   });
   test("Cartesian([4,4,4]) will be Spherical([6.92820,45.0000,54.7356])", () => {
     expect(convertCartesianToSpherical([4,4,4])).toEqual([6.9282,45.0000,54.7356]);
