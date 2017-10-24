f9fdfa71155fb21d8188ddea383a8ad069d70296
Andrey Semin <andreysemin@Andreys-MacBook-Pro.local>
2017-10-24 10:14:11 +0300

    review changes

--- test/omit.test.js
+++ test/omit.test.js
@@ -8,4 +8,20 @@ describe("omit", () => {
       c: "value"
     }, ["a", "b"])).toEqual({c: "value"});
   });
+
+  test("omit [b, c] of {a, b, c} to equal {a}", () => {
+    expect(omit({
+      a: "value",
+      b: "value",
+      c: "value"
+    }, ["b", "c"])).toEqual({a: "value"});
+  });
+
+  test("omit [d, c] of {a, b, c} to equal {a, b}", () => {
+    expect(omit({
+      a: "value",
+      b: "value",
+      c: "value"
+    }, ["d", "c"])).toEqual({a: "value", b: "value"});
+  });
 });
