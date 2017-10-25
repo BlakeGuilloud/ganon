a76360429abfb1f9bef04d8cc068aa26a1626004
N0v0s <flt@b4sh.de>
2017-10-25 23:24:06 +0200

    Determine whether node in a directed graph isReachable(). #488

---
+++ test/hasGraphCycle.test.js
@@ -0,0 +1,32 @@
+const { hasGraphCycle } = require("../lib");
+
+
+describe("hasGraphCycle", () => {
+  test("When the graph contains a cycle, then return true", () => {
+
+    let directedGraph =
+    [
+      {from: "A", to: "C"},
+      {from: "B", to: "D"},
+      {from: "C", to: "E"},
+      {from: "D", to: "A"},
+      {from: "E", to: "F"},
+      {from: "F", to: "A"}
+    ];
+
+    expect(hasGraphCycle(directedGraph)).toBe(true);
+  });
+
+  test("When the graph does not contain a cycle, then return false", () => {
+
+    let directedGraph =
+    [
+      {from: "A", to: "C"},
+      {from: "B", to: "D"},
+      {from: "C", to: "E"},
+      {from: "D", to: "A"},
+      {from: "E", to: "F"}
+    ];
+    expect(hasGraphCycle(directedGraph)).toBe(true);
+  });
+});
