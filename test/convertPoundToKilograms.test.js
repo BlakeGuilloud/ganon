db290c6b9f05fe1ce3445ea5ee7ab2bc9b032f1b
Ken K <ken@kenhkelly.us>
2017-10-25 06:47:27 -0400

    Remove the two different convertPountToKilograms because files were conflicting on the file system.

--- test/convertPoundToKilograms.test.js
+++
@@ -1,8 +0,0 @@
-const convert = require("../lib/convertPoundToKilograms");
-
-describe("convert", () => {
-  test("1 Pound is 0.45 Kilograms", () => {
-    expect(convert(1)).toBeCloseTo(0.45, 1);
-  });
-
-});
