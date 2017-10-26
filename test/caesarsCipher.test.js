6fbd050e5bec2c9f6a0e23c3a0ecbea89a20c4cd
David Kwan <davidkwan95@gmail.com>
2017-10-26 21:12:44 +0700

    Solve caesarsCipher.js and create isEmpty skeleton

--- test/caesarsCipher.test.js
+++ test/caesarsCipher.test.js
@@ -15,4 +15,12 @@ describe("caesarsCipher", () => {
     expect(caesarsCipher("HELLO WORLD!")).toEqual("URYYB JBEYQ!");
     expect(caesarsCipher("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG")).toEqual("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT");
   });
+
+  test("lower case encrypts to lower case and uppercase encrypts to uppercase", () => {
+    expect(caesarsCipher("The Quick Brown Fox Jumps Over The Lazy Dog")).toEqual("Gur Dhvpx Oebja Sbk Whzcf Bire Gur Ynml Qbt");
+  });
+
+  test("combining letters, spacing or other characters, and uppercase", () => {
+    expect(caesarsCipher("4noth3r s3cr3t m355age LUL")).toEqual("4abgu3e f3pe3g z355ntr YHY");
+  });
 });
