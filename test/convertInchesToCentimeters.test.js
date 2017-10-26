7408d3ef6cd39c0e2f836bad9b7f61a188d65530
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-10-26 17:52:51 -0500

    Fix convertInchesToCentimeters

--- test/convertInchesToCentimeters.test.js
+++ test/convertInchesToCentimeters.test.js
@@ -1,12 +1,16 @@
 const { convertInchesToCentimeters } = require("../lib");
 
 describe("convertInchesToCentimeters", () => {
-  test("5 inches is 12.7000254000508 cm", () => {
-    expect(convertInchesToCentimeters(5)).toBe(12.7000254000508);
+  test("5 inches is 12.70 cm", () => {
+    expect(convertInchesToCentimeters(5)).toBe(12.7);
   });
 
-  test("7 inches is 17.78003556007112 cm", () => {
-    expect(convertInchesToCentimeters(7)).toBe(12.78003556007112);
+  test("7 inches is 17.78 cm", () => {
+    expect(convertInchesToCentimeters(7)).toBe(17.78);
+  });
+
+  test("12 inches is 30.48 cm", () => {
+    expect(convertInchesToCentimeters(12)).toBe(30.48);
   });
 
   test("Throw error on invalid type", () => {
