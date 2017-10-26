387c61540ec04c75225cc0c654ec27af8644d10b
Josh Barker <jbarker@t1v.com>
2017-10-26 09:56:49 -0400

    updates

--- test/convertPoundToKilograms.test.js
+++ test/convertPoundToKilograms.test.js
@@ -1,8 +1,21 @@
-const convert = require("../lib/convertPoundToKilograms");
+const { convertPoundtoKilograms } = require("../lib");
 
-describe("convert", () => {
-  test("1 Pound is 0.45 Kilograms", () => {
-    expect(convert(1)).toBeCloseTo(0.45, 1);
+describe("convertPoundtoKilograms", () => {
+  test("10 pounds is 4.54 kgs",() => {
+    expect(convertPoundtoKilograms(10)).toBe(4.54);
   });
-
+  test("15 pounds is 6.80 kgs",() => {
+    expect(convertPoundtoKilograms(15)).toBe(6.80);
+  });
+  test("25 pounds is 11.3 kgs",() => {
+    expect(convertPoundtoKilograms(25)).toBe(11.3);
+  });
+  test("Throw error on invalid type",() => {
+    expect(() => convertPoundtoKilograms("ten Pounds")).toThrow("invalid type");
+    expect(() => convertPoundtoKilograms(null)).toThrow("invalid type");
+    expect(() => convertPoundtoKilograms(undefined)).toThrow("invalid type");
+    expect(() => convertPoundtoKilograms({})).toThrow("invalid type");
+    expect(() => convertPoundtoKilograms(NaN)).toThrow("invalid type");
+    expect(() => convertPoundtoKilograms(-5)).toThrow("invalid type");
+  });
 });
