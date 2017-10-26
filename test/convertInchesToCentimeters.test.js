67b77e27db4e747f87ae51f3a29f2ef13e06abc5
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-10-26 17:36:56 -0500

    Changes tests in order to be useful

--- test/convertInchesToCentimeters.test.js
+++ test/convertInchesToCentimeters.test.js
@@ -1,15 +1,18 @@
 const { convertInchesToCentimeters } = require("../lib");
 
 describe("convertInchesToCentimeters", () => {
-  it("Should return converted inches to centimeters", () => {
-    expect(() => convertInchesToCentimeters(5).toBe(12.7000254000508));
-    expect(() => convertInchesToCentimeters(7).toBe(17.78003556007112));
-    // add more cases
+  test("5 inches is 12.7000254000508 cm", () => {
+    expect(convertInchesToCentimeters(5)).toBe(12.7000254000508);
   });
 
-  it("Should check if parameter is valid", () => {
-    expect(() => convertInchesToCentimeters({})).toThrow("Invalid Argument");
-    expect(() => convertInchesToCentimeters([1,2])).toThrow("Invalid Argument");
-    // add more cases
+  test("7 inches is 17.78003556007112 cm", () => {
+    expect(convertInchesToCentimeters(7)).toBe(12.78003556007112);
+  });
+
+  test("Throw error on invalid type", () => {
+    expect(() => convertInchesToCentimeters("a inch")).toThrow("Invalid Type");
+    expect(() => convertInchesToCentimeters(null)).toThrow("Invalid Type");
+    expect(() => convertInchesToCentimeters(undefined)).toThrow("Invalid Type");
+    expect(() => convertInchesToCentimeters({})).toThrow("Invalid Type");
   });
 });
