74e4183f4a9a78e6fdd1056a2e564a73758611ef
Joseph Hunt <josephb.hunt@gmail.com>
2017-10-26 00:33:02 -0400

    Added tests for argument bounds.

--- test/areaOfCircleSector.test.js
+++ test/areaOfCircleSector.test.js
@@ -6,6 +6,12 @@ describe("areaOfCircleSector", () => {
     expect(() => areaOfCircleSector(5, "pizza")).toThrow("Invalid Argument");
   });
 
+  test("throws an error for out of bounds arguments", () => {
+    expect(() => areaOfCircleSector(5, 361)).toThrow("Argument sectorDegrees must not be greater than 360.");
+    expect(() => areaOfCircleSector(5, -1)).toThrow("Argument sectorDegrees not be less than zero.");
+    expect(() => areaOfCircleSector(-1, 60)).toThrow("Argument radius must not be less than zero.");
+  });
+
   test("returns the area of the sector", () => {
     expect(areaOfCircleSector(5, 60)).toBe(13.09);
   });
