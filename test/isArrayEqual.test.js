70b99b6c80b60264b4422e9685fc6f6a1a856f93
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2017-10-26 20:50:18 -0500

    Change arrayUnion to isArrayEqual
    Fix test suite name to match file name for isArrayEqual test suite
    
    Implement method.  Cleaned up test language.
    
    Add one more test condition to test suite

--- test/isArrayEqual.test.js
+++ test/isArrayEqual.test.js
@@ -1,22 +1,23 @@
-const { arrayUnion } = require("../lib");
+const { isArrayEqual } = require("../lib");
 
-describe("arrayUnion", () => {
-  test("both of array are equal", () => {
-    expect(arrayUnion([1,2,3],[3,2,1])).toBe(true);
-    expect(arrayUnion([11.01, 13, 7 ], [13, 7, 11.01])).toBe(true);
-    expect(arrayUnion(["Hack", "to", "ber", "fest"], ["fest", "ber", "Hack", "to"])).toBe(true);
+describe("isArrayEqual", () => {
+  test("arrays are equal", () => {
+    expect(isArrayEqual([1,2,3],[3,2,1])).toBe(true);
+    expect(isArrayEqual([11.01, 13, 7 ], [13, 7, 11.01])).toBe(true);
+    expect(isArrayEqual(["Hack", "to", "ber", "fest"], ["fest", "ber", "Hack", "to"])).toBe(true);
+    expect(isArrayEqual(["bits", 0, "bytes", 1], [1, "bits", 0, "bytes"])).toBe(true);
   });
 
-  test("both array are unequal", () => {
-    expect(arrayUnion(["this", "is", "a", "test"], ["test", "is", "cool"])).toBe(false);
-    expect(arrayUnion([1,55,7,43,2], ["hello", "whoever", "doing", "this"])).toBe(false);
-    expect(arrayUnion(["Bla", "Bla", "Bla"], [1,5,8,4,2])).toBe(false);
+  test("arrays are unequal", () => {
+    expect(isArrayEqual(["this", "is", "a", "test"], ["test", "is", "cool"])).toBe(false);
+    expect(isArrayEqual([1,55,7,43,2], ["hello", "whoever", "doing", "this"])).toBe(false);
+    expect(isArrayEqual(["Bla", "Bla", "Bla"], [1,5,8,4,2])).toBe(false);
   });
 
-  test("Throw 'Invalid Argument' when both of the parameter are not array", () => {
-    expect(() => arrayUnion("asdf", [1,2,3,4])).toThrow("Invalid Argument");
-    expect(() => arrayUnion(NaN, [1])).toThrow("Invalid Argument");
-    expect(() => arrayUnion({"test":"test"}, NaN)).toThrow("Invalid Argument");
-    expect(() => arrayUnion([1,2,3], "Hi")).toThrow("Invalid Argument");
+  test("throw 'Invalid Argument' when one or more of the parameters are not arrays", () => {
+    expect(() => isArrayEqual("asdf", [1,2,3,4])).toThrow("Invalid Argument");
+    expect(() => isArrayEqual(NaN, [1])).toThrow("Invalid Argument");
+    expect(() => isArrayEqual({"test":"test"}, NaN)).toThrow("Invalid Argument");
+    expect(() => isArrayEqual([1,2,3], "Hi")).toThrow("Invalid Argument");
   });
 });
