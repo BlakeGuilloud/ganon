5b2e257d9d894f322c6d185286b57731e7e7fbc3
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-10-28 15:16:00 -0700

    fixed linting issues

--- test/convertGigabytesToBits.test.js
+++ test/convertGigabytesToBits.test.js
@@ -5,7 +5,7 @@ describe("convertGigabytesToBits", () => {
     expect(convertGigabytesToBits(0)).toBe(0);
     expect(convertGigabytesToBits(1)).toBe(8589934592);
     expect(convertGigabytesToBits(128)).toBe(1099511627776);
-    expect(convertGigabytesToBits(69)).toBe(592705486848)
+    expect(convertGigabytesToBits(69)).toBe(592705486848);
   });
 
   test("throw error when the argument is not a number", () => {
