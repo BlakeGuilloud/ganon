8e023231f1f56783d257d93e74bdddc2aaa6deee
yang lorenzana <ylorenzana95@gmail.com>
2017-10-28 20:19:02 -0400

    Implement convertInchesToMeters method and tests (#611)

---
+++ test/primeNumbersUpTo.test.js
@@ -0,0 +1,22 @@
+const { primeNumbersUpTo } = require("../lib");
+
+describe("primeNumbersUpTo", () => {
+  test("Prime numbers <= 60", () => {
+    expect(primeNumbersUpTo(60).toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59]));
+  });
+
+  test("Prime numbers <= 30", () => {
+    expect(primeNumbersUpTo(60).toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]));
+  });
+
+  test("Throw error on invalid type", () => {
+    expect(() => primeNumbersUpTo("This is not a number!")).toThrow("Invalid type");
+    expect(() => primeNumbersUpTo([3])).toThrow("Invalid type");
+    expect(() => primeNumbersUpTo(null)).toThrow("Invalid type");
+    expect(() => primeNumbersUpTo(undefined)).toThrow("Invalid type");
+  });
+
+  test("Throw error on invalid value", () => {
+    expect(() => primeNumbersUpTo(-1)).toThrow("Invalid value");
+  });
+});
