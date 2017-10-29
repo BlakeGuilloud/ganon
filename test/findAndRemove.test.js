b5f70520068d428e1c6dea1c26fc3b88d9ca2c75
Justin Doak <justinhdoak@gmail.com>
2017-10-29 18:01:12 -0400

    Wrote findAndRemove implementation and added tests.

--- test/findAndRemove.test.js
+++ test/findAndRemove.test.js
@@ -1,17 +1,23 @@
 const { findAndRemove } = require("../lib");
 
-describe("findAndRemove returns true if atleast single occurrence was found and removed successfully", () => {
-  test("when empty array is passed", () => {
+describe("findAndRemove", () => {
+  test("returns false when empty array is passed", () => {
     expect(findAndRemove([], 4)).toEqual(false);
   });
-  test("when second argument is not passed", () => {
-    expect(findAndRemove([1, 2, 43, 5])).toThrowError("Second argument is required");
+  test("throws error when second argument is not passed", () => {
+    expect(() => findAndRemove([1, 2, 43, 5])).toThrowError("Second argument is required");
+    expect(() => findAndRemove([1, 2, 43, 5], null)).toThrowError("Second argument is required");
   });
-  test("findAndRemove returns true and removes ", () => {
+  test("returns array with second argument value removed", () => {
     expect(findAndRemove([1, 2, 3, 4, 5, 6, 76, 87, 8, 76, 4], 3)).toEqual([1, 2, 4, 5, 6, 76, 87, 8, 76, 4]);
     expect(findAndRemove([1, 2, 4, 5, 6, 54, 56, 45, 6, 4, 4], 4)).toEqual([1, 2, 5, 6, 54, 56, 45, 6]);
   });
-  test("findAndRemove returns false when there is no such number found in array", () => {
+  test("returns false when there is no such value found in array", () => {
     expect(findAndRemove([1, 2, 4, 5, 6, 54, 56, 45, 6, 4, 4], 100)).toEqual(false);
+  });
+  test("throws error when first argument is not an array", () => {
+    expect(() => findAndRemove("string")).toThrowError(TypeError);
+    expect(() => findAndRemove({})).toThrowError(TypeError);
+    expect(() => findAndRemove(1234)).toThrowError(TypeError);
   });
 });
