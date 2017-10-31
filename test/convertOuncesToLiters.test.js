9e596d670b9face6d4a67e8ea1fbbe498e0bae83
Adam Stone <a.stone2695@gmail.com>
2017-10-30 21:00:03 -0400

    Use existing isConsonant method and include edge cases in skeleton tests

--- test/convertOuncesToLiters.test.js
+++ test/convertOuncesToLiters.test.js
@@ -6,4 +6,16 @@ describe("convertOuncesToLiters", () => {
     expect(convertOuncesToLiters(10.5)).toBe(0.3105221);
     expect(convertOuncesToLiters(32)).toBe(0.946353);
   });
+
+  test("throw error when the argument is not a number", () => {
+    expect(() => convertOuncesToLiters(null)).toThrow();
+    expect(() => convertOuncesToLiters(true)).toThrow();
+    expect(() => convertOuncesToLiters(false)).toThrow();
+    expect(() => convertOuncesToLiters(undefined)).toThrow();
+    expect(() => convertOuncesToLiters([])).toThrow();
+  });
+
+  test("throw error when the input is negative", () => {
+    expect(() => convertOuncesToLiters(-1)).toThrow();
+  });
 });
