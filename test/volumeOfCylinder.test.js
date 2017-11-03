ab2f633aae46adf2a91dcc1fdba03c03072a253d
Jameson Knoll <j.knoll621@gmail.com>
2017-11-03 22:15:37 +0800

    finish unit testing for volumeOfCylinder

--- test/volumeOfCylinder.test.js
+++ test/volumeOfCylinder.test.js
@@ -2,15 +2,16 @@ const { volumeOfCylinder } = require("../lib");
 
 describe("volumeOfCylinder", () => {
 
+  test("Should return the area of the cylinder", () => {
+    expect(volumeOfCylinder(5, 10)).toBeCloseTo(785);
+  });
+
   test("Should throw an error if called with incorrect params", () => {
     expect(() => volumeOfCylinder()).toThrow();
     expect(() => volumeOfCylinder(1, "2")).toThrow();
     expect(() => volumeOfCylinder(1, [2])).toThrow();
     expect(() => volumeOfCylinder(1, undefined)).toThrow();
     expect(() => volumeOfCylinder(1, null)).toThrow();
-  });
-
-  test("Should return the area of the cylinder", () => {
-    expect(volumeOfCylinder(10, 5)).toBe(20);
+    expect(() => volumeOfCylinder(1, -1)).toThrow();
   });
 });
