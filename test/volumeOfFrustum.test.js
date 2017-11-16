69965f2bfd44c7d263cc63fc292463389147c182
Shubham Jain <jsjain007@gmail.com>
2017-11-16 20:53:19 +0530

    fixes #749 (#774)
    * fixes #357
    
    * for issue #420
    
    * removed files for issue #420
    
    * for issue #420
    
    * test case for issue #420
    
    * fixes #749
    
    * issue template for #773
    
    * fixed modification of str var

---
+++ test/volumeOfFrustum.test.js
@@ -0,0 +1,20 @@
+const { volumeOfFrustum } = require("../lib");
+
+describe("volumeOfFrustum", () => {
+
+  test("Should return the volume of frtustum of cone", () => {
+    expect(volumeOfFrustum(5, 10, 10)).toBeCloseTo(1832.59);
+  });
+
+  test("Should throw an error if called with incorrect params", () => {
+    expect(() => volumeOfFrustum()).toThrow();
+    expect(() => volumeOfFrustum(1, "2")).toThrow();
+    expect(() => volumeOfFrustum(1, [2])).toThrow();
+    expect(() => volumeOfFrustum(1, undefined, 45)).toThrow();
+    expect(() => volumeOfFrustum(1, null, 10)).toThrow();
+    expect(() => volumeOfFrustum(1, -1, 1)).toThrow();
+    expect(() => volumeOfFrustum(1, 1, -1)).toThrow();
+    expect(() => volumeOfFrustum(-1, 1, 1)).toThrow();
+    expect(() => volumeOfFrustum(1, 2)).toThrow();
+  });
+});
