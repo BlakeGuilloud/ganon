c7b4433c1b0bd4e4edfcab6bdfa1db4a2adb62cd
Kyle Tilman <kyletilman@gmail.com>
2018-10-03 09:55:35 -0400

    Fix mapArray to work as intended
    Issue #785

--- test/mapArray.test.js
+++ test/mapArray.test.js
@@ -3,21 +3,15 @@ const { mapArray } = require("../lib");
 describe("mapArray", () => {
   it("returns an array of elements transformed by a callback function", () => {
     const arr = [1,2,3,4,5,6,7,8,9,10];
-    const callback = function(elem) {
-      var result = [];
-      for (var i = 0; i < elem.length; i++) {
-        result.push(elem[i] ** 2);
-      }
-      return result;
-    };
+    const callback = function(elem) { return elem ** 2; };
     expect(mapArray(arr, callback)).toEqual([1,4,9,16,25,36,49,64,81,100]);
   });
 
   it("does not mutate the input array", () => {
-    const arr = [["h","e","l","l","o"], ["h","o","w"], ["a","r","e"], ["y","o","u","?"]];
-    const callback = function(elem) { return elem.reverse(); };
+    const arr = [1,2,3,4,5,6];
+    const callback = function(elem) { return ++elem; };
     mapArray(arr, callback);
-    expect(arr).toEqual([["h","e","l","l","o"], ["h","o","w"], ["a","r","e"], ["y","o","u","?"]]);
+    expect(arr).toEqual([1,2,3,4,5,6]);
   });
 
   it("throws an error if the first argument is not an array", () => {
